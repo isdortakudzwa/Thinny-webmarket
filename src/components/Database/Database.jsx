fd1a40af1cdd067449867bbf8eaece64f411b37e
Isdor Taku <isdortakudzwamunengwa@gmail.com>
2024-10-23 13:40:38 +0200

    Best web app

--- src/components/Database/Database.jsx
+++ src/components/Database/Database.jsx
@@ -8,12 +8,6 @@ import huji from '../Database/Images/others 1.jpg';
 import green from '../Database/Images/bhoe.jpg';
 import reng from '../Database/Images/reng.jpg';
 import chuch from '../Database/Images/colors.jpg';
-import pep from './Images/vivo.png';
-import pepo from './Images/set.png';
-import pepy from './Images/lima2.png';
-import pepiy from './Images/lima3.png';
-import pepyo from './Images/mimi.jpg';
-import repy from './Images/lima4.png'
 import star from './Images/285661_star_icon.png'
 
 const products = [
@@ -61,16 +55,16 @@ const products = [
     rate:star
   },
 
-  {
-    id: 80,
-    img:  pepo,
-    name: 'Nike Boots',
-    text:'High-quality Nike boots that offer a perfect balance between rugged functionality and street style.',
-    btn:'Buy',
-    price: 89.99,
-    sponser:"https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png",
-    rate:star
-  },
+  // {
+  //   id: 80,
+  //   img:  pepo,
+  //   name: 'Nike Boots',
+  //   text:'High-quality Nike boots that offer a perfect balance between rugged functionality and street style.',
+  //   btn:'Buy',
+  //   price: 89.99,
+  //   sponser:"https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png",
+  //   rate:star
+  // },
 
   {
     id: 5,
@@ -83,6 +77,7 @@ const products = [
     rate:star
   },
 
+ 
   {
     id: 6,
     img: chuch,
@@ -161,81 +156,48 @@ const products = [
   },
 
   {
-    id: 81,
-    img:  repy,
-    name: 'Nike Boots',
-    text:'Designed for performance, these Nike boots offer both protection and style on the go.',
+    id: 44,
+    img: purple,
+    name: 'Sneakers Air force',
+    text:'The perfect blend of fashion and function, these Air Force sneakers feature a sleek design.',
     btn:'Buy',
-    price: 89.99,
+    price: 49.99,
     sponser:"https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png",
     rate:star
   },
-
   {
-    id: 83,
-    img:  pepiy,
+    id: 243,
+    img: pink,
     name: 'Nike Boots',
-    text:'High-performance Nike boots that combine durability with cutting-edge design.',
+    text:'Durable Nike boots built for both rugged outdoor activities and casual city walks.',
     btn:'Buy',
     price: 89.99,
-    sponser:"https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png",
-    rate:star
-  },
-  
-  {
-    id: 84,
-    img:   pepyo,
-    name: 'Nike Boots',
-    text:'A modern take on classic Nike boots, offering a sleek silhouette and all-day comfort.',
-    btn:'Buy',
-    price: 89.99,
-    sponser:"https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png",
+    sponser:"https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png",
     rate:star
   },
 
   {
-    id: 85,
-    img:  pep,
+    id: 409,
+    img: colors,
     name: 'Nike Boots',
-    text:'These Nike boots provide excellent support and grip, perfect for active lifestyles.',
+    text:'Stylish and sturdy, these Nike boots are designed to provide maximum comfort and durability.',
     btn:'Buy',
     price: 89.99,
     sponser:"https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png",
     rate:star
   },
+ 
 
   {
-    id: 87,
-    img:  pepy,
-    name: 'Nike Boots',
-    text:'Rugged and stylish, these Nike boots are made for both performance and style.',
+    id: 1,
+    img: others,
+    name: 'Sneakers Air force',
+    text:'A classic pair of Air Force sneakers, offering exceptional comfort and style for everyday wear.',
     btn:'Buy',
-    price: 89.99,
-    sponser:"https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png",
+    price: 49.99,
+    sponser:"https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png",
     rate:star
   },
-
-  {
-    id: 86,
-    img:  pepy,
-    name: 'Nike Boots',
-    text:'Whether hiking or walking around town, these Nike boots offer reliable comfort and durability.',
-    btn:'Buy',
-    price: 89.99,
-    sponser:"https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png",
-    rate:star
-  },
-  
-  {
-    id: 97,
-    img:  pepy,
-    name: 'Nike Boots',
-    text:'These premium Nike boots provide exceptional comfort and durability for long-term wear.',
-    btn:'Buy',
-    price: 89.99,
-    sponser:"https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png",
-    rate:star
-  }
 
 ];
 
--- src/pages/GetSignedUp.jsx
+++ src/pages/GetSignedUp.jsx
@@ -1,72 +1,60 @@
 import { useState } from 'react';
 //  import now2 from '../components/Database/Images/now1 - Copy.png';
- import one from '../components/Database/Images/nhasi.png';
- import two from '../components/Database/Images/nhasi1.png';
- import three from '../components/Database/Images/nhasi3.png';
- import plus from '../components/Database/Images/plus_5345954.png';
- import { Link } from 'react-router-dom';
-
+import one from '../components/Database/Images/nhasi.png';
+import two from '../components/Database/Images/nhasi1.png';
+import three from '../components/Database/Images/nhasi3.png';
+import plus from '../components/Database/Images/plus_5345954.png';
+import { Link } from 'react-router-dom';
 
 function GetSignedUp() {
-
   
-const [shoe, setShoe] = useState ([
+  const [shoe, setShoe] = useState([
+    {
+      id: '09',
+      img: two,
+      text: "Balenciaga One",
+      smallText: "Balenciaga One is known for its unique, chunky design and luxurious comfort. A top choice for sneakerheads seeking a bold statement in style."
+    },
+    {
+      id: '25',
+      img: one,
+      text: "Sock Boots",
+      smallText: "Sock Boots offer a snug, sleek fit with an ultra-modern look. These boots combine comfort with a high-fashion aesthetic, perfect for urban wear."
+    },
+    {
+      id: '323',
+      img: three,
+      text: "The Mega",
+      smallText: "The Mega brings a futuristic silhouette with exaggerated soles, providing maximum support and standout style for those who want to be noticed."
+    }
+  ]);
 
-  {
-    id:'09',
-    img:two,
-    text:"Balanciaga One",
-    smallText:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, quo aliquid dolorem harum ex consectetur."
-
-  },
-  
-
-  {
-    id:'25',
-    img:one,
-    text:"Sock  Boots",
-    smallText:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, quo aliquid dolorem harum ex consectetur."
-
-  },
-
-  {
-    id:'323',
-    img:three,
-    text:"The Mega",
-    smallText:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, quo aliquid dolorem harum ex consectetur."
-
+  const handleClick = () => {
+    setShoe;
   }
-
-])
-
-const handleClick = () => {
-  setShoe;
-}
 
   return (
     <div className="saini">
       <h3>Popular Products</h3>
-        <div className="piks">
-          
-          {shoe.map((sup)=> 
-             <div className="pikis" key={sup.id}>
-               <img src={sup.img} alt="shoe pic" className='piks--img'  loading="lazy" />
-               <h1>{sup.text}</h1>
-               <p onClick={handleClick}>{sup.smallText}</p>
+      <div className="piks">
+        {shoe.map((sup) => 
+          <div className="pikis" key={sup.id}>
+            <img src={sup.img} alt="shoe pic" className='piks--img' loading="lazy" />
+            <h1>{sup.text}</h1>
+            <p onClick={handleClick}>{sup.smallText}</p>
 
-               <div className="adds">
-                <p>$123</p>
-                <Link to='/Services'>
-                <img src={plus} alt="" className='plus-img'    loading="lazy" />
-                </Link>
-               </div>
+            <div className="adds">
+              <p>$123</p>
+              <Link to='/Services'>
+                <img src={plus} alt="" className='plus-img' loading="lazy" />
+              </Link>
+            </div>
 
-             </div>
-          )}
-
-        </div>
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default GetSignedUp
+export default GetSignedUp;
